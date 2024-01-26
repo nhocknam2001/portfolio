6b2247e89a777712645b0f016030cc8412ddb4db
nhocknam2001 <77433450+nhocknam2001@users.noreply.github.com>
2024-01-26 17:22:02 +0700

    Navbar, Menu, Slidebar

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,31 @@
+import "./topbar.scss";
+import { Mail, Person } from "@material-ui/icons";
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            Portfolio.
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>0983 673 974</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>trantuananh0705@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
