7d0a3f190bbcad56dcc7a88a6dcbff2fc48caf63
nhocknam2001 <77433450+nhocknam2001@users.noreply.github.com>
2024-01-31 09:33:33 +0700

    Done Portfolio + Update Work

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,38 @@
+import { useEffect, useRef } from "react";
+import { init } from "ityped";
 import "./intro.scss";
 
 export default function Intro() {
+  // Text typing animation using useEffect() + ityped
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      backDelay: 1500,
+      backSpeed: 60,
+      showCursor: true,
+      strings: ["Internship", "Front-end Developer"],
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
-      Intro
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/me.jpg"></img>
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Tuan-Anh Tran</h1>
+          <h3>
+            S.Tech <span ref={textRef}>Front-end Developer</span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" />
+        </a>
+      </div>
     </div>
   );
 }
