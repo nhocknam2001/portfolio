05bb2aa7fb0fb6a05142fac4c44e627f58bcedf7
nhocknam2001 <77433450+nhocknam2001@users.noreply.github.com>
2024-01-31 09:53:12 +0700

    Done Works component

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./works.scss";
 
 export default function Works() {
+  const [currentSlide, setCurrentSlide] = useState(0);
   const data = [
     {
       id: "1",
@@ -25,20 +26,30 @@ export default function Works() {
       img: "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
     },
   ];
+
+  const handleClick = (way) => {
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
+
   return (
     <div className="works" id="works">
-      <div className="slider">
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
         {data.map((d) => (
           <div className="container">
             <div className="item">
               <div className="left">
                 <div className="leftContainer">
                   <div className="imgContainer">
-                    <img src={d.img} alt=""></img>
+                    <img src={d.icon} alt="" />
                   </div>
                   <h2>{d.title}</h2>
                   <p>{d.desc}</p>
-                  <span>Project</span>
+                  <span>Projects</span>
                 </div>
               </div>
               <div className="right">
@@ -51,8 +62,18 @@ export default function Works() {
           </div>
         ))}
       </div>
-      <img src="assets/arrow.png" className="arrow left" alt="" />
-      <img src="assets/arrow.png" className="arrow right" alt="" />
+      <img
+        src="assets/arrow.png"
+        className="arrow left"
+        alt=""
+        onClick={() => handleClick("left")}
+      />
+      <img
+        src="assets/arrow.png"
+        className="arrow right"
+        alt=""
+        onClick={() => handleClick()}
+      />
     </div>
   );
 }
